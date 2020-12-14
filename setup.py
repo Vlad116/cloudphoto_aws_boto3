7a327cdcb4e9a0d4f523ab058410c7fc3f901083
Vladislav <vladalekseev9@yandex.ru>
2020-12-15 01:10:47 +0300

    :memo: fix readme!

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
       name='cloudphoto',
       # mandatory
       version='0.1',
-      description='cli programm for download/upload photo album',
+      description='cli script for download/upload photo album',
       # mandatory
       author='Vladislav Alekseev',
       author_email='vladalekseev9@yandex.ru',
       packages=['cloudphoto'],
       package_data={},
-      install_requires=['boto3', 'botocore','redis-py-cluster', 'logging', 'requests', 'click'],
+      install_requires=['boto3', 'botocore', 'redis-py-cluster', 'logging', 'requests', 'click'],
       entry_points={
         'console_scripts': ['cloudphoto = cloudphoto.cli:start']
       }
